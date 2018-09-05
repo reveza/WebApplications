0363f20704563540886a6476511da997528f6e81
reveza <rebeccavezinacote@gmail.com>
2018-09-05 17:10:06 -0400

    Add background color when checked box

--- TP1/calendrier.js
+++ TP1/calendrier.js
@@ -1,8 +1,15 @@
-function checkButton(myId) {
-    var img = document.getElementById(myId);
+function checkButton(image, header, number) {
+    var img = document.getElementById(image);
+    var head = document.getElementById(header);
+    var num = document.getElementById(number);
+
     if (img.src.includes("/check.png")) {
         img.src = "tick-check.png";
+        head.style.backgroundColor = "#EBF7D4";
+        num.style.backgroundColor = "#EBF7D4";
     } else {
         img.src = "check.png";
+        head.style.backgroundColor = "#FFFFFF";
+        num.style.backgroundColor = "#FFFFFF";
     }
 }
